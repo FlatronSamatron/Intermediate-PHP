baddb2d7b53c5baf1687337ffbb63eb397a9bdc6
Vitalii <flatronsamatron@gmail.com>
2024-04-14 19:22:54 +0300

    Autoloading

--- src/public/index.php
+++ src/public/index.php
@@ -1,15 +1,33 @@
 <?php
 
-require_once '../PaymentGateway/Stripe/Transaction.php';
-require_once '../PaymentGateway/Paddle/Transaction.php';
-require_once '../PaymentGateway/Paddle/CustomerProfile.php';
+//require_once '../app/PaymentGateway/Stripe/Transaction.php';
+//require_once '../app/PaymentGateway/Paddle/Transaction.php';
+//require_once '../app/PaymentGateway/Paddle/CustomerProfile.php';
+//require_once '../app/Notification/Email.php';
 
-use PaymentGateway\Stripe\Transaction as StripeTransaction;
-use PaymentGateway\Paddle\{Transaction as PaddleTransaction, CustomerProfile};
+use App\PaymentGateway\Paddle\{CustomerProfile};
+use App\PaymentGateway\Paddle\Transaction as PaddleTransaction;
+use App\PaymentGateway\Stripe\Transaction as StripeTransaction;
+use App\Notification\Email;
+
+//spl_autoload_register(function($class) {
+//    $path = __DIR__ . '/../' . lcfirst(str_replace('\\', '/', $class). '.php');
+//    if(file_exists($path)){
+//        require $path;
+//    }
+//});
+
+require __DIR__ . '/../vendor/autoload.php';
 
 var_dump(new StripeTransaction());
 echo '<hr/>';
 var_dump(new PaddleTransaction());
 echo '<hr/>';
 var_dump(new CustomerProfile());
+echo '<hr/>';
+var_dump(new Email());
+echo '<hr/>';
+
+$id = new \Ramsey\Uuid\UuidFactory();
+echo $id->uuid4();
 
