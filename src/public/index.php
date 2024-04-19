0a4b55e57faff408c0f455f975ae31d34b56ef19
Vitalii <flatronsamatron@gmail.com>
2024-04-19 15:41:52 +0300

    Magic Methods

--- src/public/index.php
+++ src/public/index.php
@@ -4,10 +4,22 @@ use App\PaymentGateway\Stripe\Transaction;
 
 require __DIR__.'/../vendor/autoload.php';
 
-$service = new \App\DebtCollectionService();
-$service->collectionDebt(new \App\CollectionAgency());
-echo '<hr/>';
-$service->collectionDebt(new \App\Rocky());
+use App\Invoice;
+
+$invoice = new Invoice();
+var_dump($invoice);
+
+//$invoice(); //__invoke
+//echo $invoice //__toString;
+
+//$invoice->process(15, 'some description'); //__call
+//echo '<hr/>';
+//App\Invoice::process(1, 2, 3);
+//$invoice->ammount = 50;
+//
+//var_dump(isset($invoice->ammount));
+//unset($invoice->ammount);
+//var_dump(isset($invoice->ammount));
 
 echo '<hr/>';
 echo Transaction::class;
