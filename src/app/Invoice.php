a88c5e437a6417833868af45903f89f383674f81
Vitalii <flatronsamatron@gmail.com>
2024-04-21 18:32:00 +0300

    Object Cloning

---
+++ src/app/Invoice.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+namespace App;
+
+class Invoice
+{
+    private string $id;
+
+    public function __construct()
+    {
+        $this->id = uniqid('invoice_');
+    }
+
+    public static function create(): static
+    {
+        return new static();
+    }
+
+    public function __clone(): void
+    {
+        $this->id = uniqid('invoice_');
+    }
+}
--- src/public/index.php
+++ src/public/index.php
@@ -2,6 +2,13 @@
 
 require __DIR__.'/../vendor/autoload.php';
 
+$invoice  = new \App\Invoice();
+$invoice2 = new $invoice();
+$invoice3 = clone $invoice2;
+
+echo '<pre>';
+var_dump($invoice, $invoice2, $invoice3, \App\Invoice::create(), $invoice3 === $invoice2);
+echo '<pre/>';
 
 echo '<hr/>';
 
