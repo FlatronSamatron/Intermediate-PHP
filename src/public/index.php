713680efd8798a6323cfe6b1a716e4ebde4be8c1
Vitalii <flatronsamatron@gmail.com>
2024-04-29 22:59:49 +0300

    DateTime

--- src/public/index.php
+++ src/public/index.php
@@ -1,21 +1,39 @@
 <?php
 
 require __DIR__.'/../vendor/autoload.php';
+$date = '15/05/2020 3:30PM';
 
-use App\{Invoice, Customer};
+$dateTime = DateTime::createFromFormat('d/m/Y g:iA', $date);
 
-$invoice = new Invoice(new Customer(['transaction1' => '50']));
+echo '<pre>';
+var_dump($dateTime);
+echo '<pre/>';
 
-try {
-    $invoice->process(-25);
-} catch (\App\Exception\MissingBillingInfoException $e) {
-    echo $e->getMessage().' '.$e->getFile().' '.$e->getLine();
-} catch (\InvalidArgumentException) {
-    echo 'Invalid argument exception';
-} finally {
-    echo '<br/>';
-    echo 'Finally block';
-}
+$dateTime = new DateTime('', new DateTimeZone('Europe/Kyiv'));
+$dateTime->setDate(2020, 4, 21)->setTime(2, 15);
+
+echo '<pre>';
+var_dump($dateTime->format('m/d/Y g:i A'));
+echo '<pre/>';
 
 echo '<hr/>';
+
+$dateTime1 = new DateTime('05/25/2020 3:30PM');
+$dateTime2 = new DateTime('03/15/2020 3:30PM');
+var_dump($dateTime1 < $dateTime2);
+var_dump($dateTime1 > $dateTime2);
+var_dump($dateTime1 == $dateTime2);
+var_dump($dateTime1 <=> $dateTime2);
+echo '<hr/>';
+var_dump($dateTime1->diff($dateTime2));
+echo '<hr/>';
+
+$period = new DatePeriod(new DateTime('05/01/2020'), new DateInterval('P1D'), new DateTime('05/31/2020'));
+
+foreach ($period as $date) {
+    echo $date->format('m/d/Y');
+    echo '<br/>';
+}
+
+
 
