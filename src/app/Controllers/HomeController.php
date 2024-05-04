1575356aa837171bf4a03fdfb0ea0446257e2fd9
Vitalii <flatronsamatron@gmail.com>
2024-05-04 20:35:14 +0300

    PDO Part 1 - Prepared Statements - SQL Injection

--- src/app/Controllers/HomeController.php
+++ src/app/Controllers/HomeController.php
@@ -4,28 +4,47 @@ declare(strict_types=1);
 namespace App\Controllers;
 
 use App\View;
+use PDO;
 
 class HomeController
 {
     public function index(): View
     {
+        try {
+            $db        = new PDO('mysql:host=db;dbname=my_db', 'root', 'root', [
+                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+                    PDO::ATTR_EMULATE_PREPARES   => false,
+            ]);
+            $email     = 'oleg@mail.com';
+            $name      = 'oleg oleg';
+            $isActive  = 1;
+            $createdAt = date('Y-m-d H:m:i', strtotime('07/11/2020 9:00PM'));
+            $query     = "INSERT INTO users (email, full_name, is_active, created_at, update_at) VALUES (:email, :name, :active, :date1, :date2)";
+
+            $stmt = $db->prepare($query);
+
+            $stmt->bindValue(':email', $email);
+            $stmt->bindValue(':name', $name);
+            $stmt->bindValue(':active', $isActive, PDO::PARAM_BOOL);
+            $stmt->bindValue(':date1', $createdAt);
+            $stmt->bindValue(':date2', $createdAt);
+
+//            echo $stmt;
+
+            $stmt->execute();
+
+            $id = (int)$db->lastInsertId();
+
+            foreach ($db->query('select * from users')->fetchAll() as $user) {
+                echo '<pre>';
+                var_dump($user);
+                echo '</pre>';
+            }
+        } catch (\PDOException $e) {
+            throw new \PDOException($e->getMessage(), (int)$e->getCode());
+        }
+
+
         return View::make('index', ['foo' => 'bar']);
-    }
-
-    public function upload(): void
-    {
-        echo '<pre>';
-        var_dump($_FILES);
-        var_dump(pathinfo($_FILES['receipt']['tmp_name']));
-        echo '</pre>';
-        $file     = $_FILES['receipt']['name'];
-        $filePath = STORAGE_PATH.'/'.$file;
-        echo '<pre>';
-        var_dump(pathinfo($filePath));
-        echo '</pre>';
-
-        move_uploaded_file($_FILES['receipt']['tmp_name'], $filePath);
-
-        header('Location : /');
     }
 }
